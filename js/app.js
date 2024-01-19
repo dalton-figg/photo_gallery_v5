f706122976c8abd699569b092800156d44139f93
Dalton <dalton@stormcreative.co.uk>
2024-01-19 12:27:01 +0000

    Add lightbox functionality

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 // setup fitler
-const search = new Filter('search', 'data-caption');
+const search = new Filter("search", "data-caption");
+// setup lightbox
+window.addEventListener("load", function () {
+  baguetteBox.run(".gallery");
+});
